741ad9961ddf87687bbf170da886d0bb951e6f37
NathanStein1 <nathanstein2@gmail.com>
2022-05-25 13:56:08 -0700

    Created more prompts and functions

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const inquirer = require("inquirer")
 
 const fs = require('fs')
-const Employee = require('./lib/Employee')
-const Manager = require('./lib/Manager')
-const Engineer = require('./lib/Engineer')
-const Intern = require('./lib/intern')
+// const Employee = require('./lib/Employee')
+// const Manager = require('./lib/Manager')
+// const Engineer = require('./lib/Engineer')
+// const Intern = require('./lib/intern')
 
-    // Begin inquierer prompts
-    // .THEN, create child element and append it to the cardbox div, use flex properties and padding in css
-    // 
+// Begin inquierer prompts
+// .THEN, create child element and append it to the cardbox div, use flex properties and padding in css
 
-inquirer
-    .prompt([
 
-        {
-            type: 'list',
-            message: 'Role?',
-            name: 'role',
-            choices: ['Manager', 'Employee', 'Engineer', 'Intern']
-        },
+
+// Need to add a function that pushes the responses from the prompts into a the class array
+
+// CREATES THE INTERN
+
+function createIntern() {
+
+    inquirer.prompt([
+
         {
             type: 'input',
             message: 'Name?',
@@ -30,69 +30,351 @@ inquirer
             message: 'ID number?',
             name: 'id',
         },
-        // Ask what role, then I get the boilerplante name, etc, 2nd prompt gets unique information
+
         {
             type: 'input',
             message: 'What is your email?',
             name: 'email',
         },
+
+        {
+
+            type: 'input',
+            message: 'What school do you attend?',
+            name: 'school',
+        },
+    ])
+        .then((response) => {
+            console.log(response)
+            createTeam()
+        })
+}
+
+
+
+// CREATES THE ENGINEER INFO
+function createEngineer() {
+
+    inquirer.prompt([
+
         {
             type: 'input',
-            message: 'Room number?',
-            name: 'room',
+            message: 'Name?',
+            name: 'name',
         },
+
         {
+            type: 'input',
+            message: 'ID number?',
+            name: 'id',
+        },
+
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+        },
+
+        {
+
             type: 'input',
             message: 'Github?',
             name: 'github',
         },
+    ])
+        .then((response) => {
+            console.log(response)
+            createTeam()
+        })
+}
+
+
+function createTeam() {
+    inquirer.prompt([
+        {
+
+            type: 'list',
+            message: 'Add a new employee?',
+            name: 'moreEmployee',
+            choices: ['Engineer', 'Intern', 'No thanks']
+
+        }])
+        .then((response) => {
+            console.log(response)
+            if (response.moreEmployee == 'Engineer') {
+                createEngineer()
+
+            };
+            if (response.moreEmployee == 'Intern'){
+                createIntern()
+            }
+            else{
+                console.log('Finished')
+            }
+        });
+
+}
+
+function createManager() {
+
+    inquirer.prompt([
+
         {
             type: 'input',
-            message: 'School?',
-            name: 'school',
+            message: 'Name?',
+            name: 'name',
+        },
+
+        {
+            type: 'input',
+            message: 'ID number?',
+            name: 'id',
+        },
+
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+        },
+
+        {
+
+            type: 'input',
+            message: 'Office Number?',
+            name: 'office',
         },
     ])
-    // We promise there is a response, then once we get it, we can use that information to create a card
-    .then((response) => {
-        console.log(response)
-
-// We are using the file system to create a new file that will contain all the information we need
-        fs.writeFile('firstprompt.md', `${response.name}\n ${response.role}\n ${response.id}\n ${response.email}\n ${response.room}\n ${response.github}\n`,
-            (err) =>
-                err ? console.error(err) : console.log('Commit logged!'));
+        .then((response) => {
+            console.log(response)
+            createTeam()
+        })
 
 
-    });
+}
+
+createManager()
 
 
 
 
 
 
-        
-    let ask = () => {
-        // Prompt role, if x, than...prompt proper prompt for mang, emp, intern etc, store into an [...array]
-        }
 
 
-    let creation = () => {
-            // Store items in an array, grab items and append them to the parent element, make new divs
-            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CODE WE DON"T NEED YET/ANYMORE. Reference
+
+function printerTest() {
+
+    fs.writeFile('index.html', `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./reset.css">
+        <link rel="stylesheet" href="./style.css">
+        <title>My Team</title>
+    </head>
+    <body>
+        <header>
+            My Team
+        </header>
     
+        <main>
+            
+    
+                <div class="cardbox">
+    
+    
+                    <div class="cardManage">
+                        <div class="head">
+                        <div class="name">Bryan</div>
+                        <div class="role">Manager</div>
+                        </div>
+                        <div class="details">
+                            <div class="idnum">ID: 3</div>
+                            <div class="email">ryanbot@email.com</div>
+                            <div class="office">Room num: 1</div>
+                        </div>
+    
+                    </div>
+    
+                    <div class="card">
+                        <div class="head">
+                        <div class="name">Ryan</div>
+                        <div class="role">Intern</div>
+                        </div>
+                        <div class="details">
+                            <div class="idnum">ID: 3</div>
+                            <div class="email">ryanbot@email.com</div>
+                            <div class="github">github/ryantheman.com</div>
+                        </div>
+    
+                    </div>
+    
+    <div class="testzone">
+    </div>
+    
+    
+                </div>
+            
+    
+        </main>
+    
+        <script src="./index.js"></script>
+    </body>
+    
+    </html>`,
+        (err) =>
+            err ? console.error(err) : console.log('Commit Logged!'));
+}
 
 
 
 
 
-// Make classes, put the classes into [...array] create class via constructor
+// inquirer
+//     .prompt([
 
-// function create manager,
+//         {
+//             type: 'list',
+//             message: 'Role?',
+//             name: 'role',
+//             choices: ['Manager', 'Employee', 'Engineer', 'Intern']
+//         },
+//         {
+//             type: 'input',
+//             message: 'Name?',
+//             name: 'name',
+//         },
 
-// function create team,
+//         {
+//             type: 'input',
+//             message: 'ID number?',
+//             name: 'id',
+//         },
 
-// inquierer {
+//         {
+//             type: 'input',
+//             message: 'What is your email?',
+//             name: 'email',
+//         },
+//         {
+//             type: 'input',
+//             message: 'Room number?',
+//             name: 'room',
+//         },
+//         {
+//             type: 'input',
+//             message: 'Github?',
+//             name: 'github',
+//         },
+//         {
+//             type: 'input',
+//             message: 'School?',
+//             name: 'school',
+//         },
+//     ])
+//     // We promise there is a response, then once we get it, we can use that information to create a card
+//     .then((response) => {
+//         console.log(response)
 
-//     addEngineer
-//     addIntern
-//     switch SVGPatternElement
-// }
+// // We are using the file system to create a new file that will contain all the information we need
+//         fs.writeFile('firstprompt.md', `${response.name}\n ${response.role}\n ${response.id}\n ${response.email}\n ${response.room}\n ${response.github}\n`,
+//             (err) =>
+//                 err ? console.error(err) : console.log('Commit logged!'));
+
+
+//     });
